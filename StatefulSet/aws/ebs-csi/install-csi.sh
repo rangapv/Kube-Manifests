fbe03c89e8b74f30ca6e4bd02964e29ca4747f99
rangapv <rangapv@yahoo.com>
2023-05-16 13:21:33 +0000

    Removed the exit which was erraneously not installing the csi-driver after creating the aws-secret

--- StatefulSet/aws/ebs-csi/install-csi.sh
+++ StatefulSet/aws/ebs-csi/install-csi.sh
@@ -101,7 +101,6 @@ then
 else
 sce1=`kubectl create secret generic aws-secret --namespace kube-system --from-literal "key_id=${AWS_ACCESS_KEY_ID}" --from-literal "access_key=${AWS_SECRET_ACCESS_KEY}"`
 	echo "Successfully created Secret aws-secret "
-	exit
 fi
 
 }
